bda4be76e16c3b30dcdec40b6ce35b8c700b7097
Keshy31 <keshavmohabir@gmail.com>
2025-05-07 23:29:28 +0200

    New approach with prev and next pointers

--- 605-can-place-flowers.py
+++ 605-can-place-flowers.py
@@ -6,20 +6,32 @@ class Solution:
         print(flowerbed)
         if not n:
             return True
+
         p = 0
+
         while p < len(flowerbed):
-            if flowerbed[p] == 0 and flowerbed[p - 1] != 1 and flowerbed[p + 1] != 1:
+            if p == 0:
+                prev = 0
+            else:
+                prev = flowerbed[p - 1]
+
+            curr = flowerbed[p]
+            if p == len(flowerbed) - 1:
+                next = 0
+            else:
+                next = flowerbed[p + 1]
+
+            if (curr + prev + next) == 0:
                 flowerbed[p] = 1
-                n = n - 1
-                if n == 0:
-                    return True
-            print(flowerbed[p])
+                n -= 1
+            if n == 0:
+                return True
             p += 1
         return False
 
 
-flowerbed = [1, 0, 0, 0, 0, 1]
-n = 2
+flowerbed = [0]
+n = 1
 
 start_time = time.perf_counter_ns()
 solution = Solution()
