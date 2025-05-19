1cbe1bfa499d1d49f0e24843845a1fc004342bcc
Keshy31 <keshavmohabir@gmail.com>
2025-05-19 18:42:34 +0200

    Set up problem

---
+++ 1491-average-salary-excluding-min-max-salary.py
@@ -0,0 +1,14 @@
+class Solution:
+    def average(self, salary: list[int]) -> float:
+        print(f"Salary = {salary}")
+        minimum = min(salary)
+        print(f"Min = {minimum}")
+        maximum = max(salary)
+        print(f"Max = {maximum}")
+        length = len(salary)
+        print(f"N = {length}")
+
+        return 1.0
+
+
+salary = [4000, 3000, 1000, 2000]
