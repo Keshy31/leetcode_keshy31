2033f3bb621a4cde2a37a16d3de30d482ed4d469
Keshy31 <keshavmohabir@gmail.com>
2025-05-19 18:46:15 +0200

    Calculate result

--- 1491-average-salary-excluding-min-max-salary.py
+++ 1491-average-salary-excluding-min-max-salary.py
@@ -1,6 +1,8 @@
 class Solution:
     def average(self, salary: list[int]) -> float:
         print(f"Salary = {salary}")
+        total = sum(salary)
+        print(f"Sum = {total}")
         minimum = min(salary)
         print(f"Min = {minimum}")
         maximum = max(salary)
@@ -8,7 +10,11 @@ class Solution:
         length = len(salary)
         print(f"N = {length}")
 
+        print(f"Result = {(total - minimum - maximum) / (length - 2)}")
         return 1.0
 
 
-salary = [4000, 3000, 1000, 2000]
+salary = [1000, 2000, 3000]
+
+solution = Solution()
+solution.average(salary)
