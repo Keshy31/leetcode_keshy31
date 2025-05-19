f689d3abe5d4b401e186f25fb580f454ada784f5
Keshy31 <keshavmohabir@gmail.com>
2025-05-19 17:03:27 +0200

    Adjust sign

--- 54-spiral-matrix.py
+++ 54-spiral-matrix.py
@@ -28,14 +28,21 @@ class Solution:
                 bottom -= 1
             if left < right:
                 for i in range(bottom - top):
-                    result.append(matrix[bottom - 1 + i][left])
+                    result.append(matrix[bottom - 1 - i][left])
                     print(result)
                 left += 1
 
         return result
 
 
-matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+    [17, 18, 19, 20],
+    [21, 22, 23, 24],
+]
 
 start_time = time.perf_counter_ns()
 
