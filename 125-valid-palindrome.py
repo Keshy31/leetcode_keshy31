acdf29f3d5078008a464f9bdf47184ef4522fd67
Keshy31 <keshavmohabir@gmail.com>
2025-05-27 23:15:11 +0200

    Adjust alphanumeric number range check

--- 125-valid-palindrome.py
+++ 125-valid-palindrome.py
@@ -1,12 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s.lower()
         left = 0
         right = len(s) - 1
+
         while left < right:
+            print("gello")
             while not self.isAlphaNumeric(s[left]):
+                print("test 1")
                 left += 1
             while not self.isAlphaNumeric(s[right]):
+                print("test 2")
                 right -= 1
             print(f"{s[left]} is alphanumeric -> {self.isAlphaNumeric(s[left])}")
             print(f"{s[right]} is alphanumeric -> {self.isAlphaNumeric(s[right])}")
@@ -19,16 +22,17 @@ class Solution:
         return True
 
     def isAlphaNumeric(self, char: str) -> bool:
+        print(f"Char is {char} and ord is {ord(char)} ")
         if ord(char) in range(65, 90):
             return True
         elif ord(char) in range(97, 122):
             return True
-        elif ord(char) in range(48, 57):
+        elif ord(char) in range(48, 58):
             return True
         else:
             return False
 
 
-s = "A man, a plan, a canal: Panama"
+s = "9,8"
 solution = Solution()
-solution.isPalindrome(s)
+print(solution.isPalindrome(s))
