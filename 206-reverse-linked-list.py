c929e91c6587964034bdcdc456e3b3f3b2932564
Keshy31 <keshavmohabir@gmail.com>
2025-07-11 14:32:01 +0200

    attempt linked list

---
+++ 206-reverse-linked-list.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return head
+
+        print(head.val)
+        print_list(head.next)
+        head.next = None
+        print_list(head)
+
+        return head
+
+
+# Helper function to print a linked list (for debugging)
+def print_list(head: Optional[ListNode]):
+    current = head
+    while current:
+        print(current.val, end=" -> ")
+        current = current.next
+    print("None")
+
+
+head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, None)))))
+
+solution = Solution()
+merged = solution.reverseList(head)
+print_list(merged)
